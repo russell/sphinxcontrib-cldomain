a9a5b26b2a0d75fecf66043bcc9369164fd9a824
Russell Sim <russell.sim@gmail.com>
2012-04-21 22:08:57 +1000

    added correct licensing, it was very inconsistant before

--- sphinxcontrib/__init__.py
+++ sphinxcontrib/__init__.py
@@ -1,4 +1,19 @@
 # -*- coding: utf-8 -*-
+# cldomain is a Common Lisp domain for the Sphinx documentation tool.
+# Copyright (C) 2011-2012 Russell Sim <russell.sim@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
     sphinxcontrib
     ~~~~~~~~~~~~~
@@ -6,9 +21,6 @@
     This package is a namespace package that contains all extensions
     distributed in the ``sphinx-contrib`` distribution.
 
-    :copyright: Copyright 2007-2009 by the Sphinx team, see AUTHORS.
-    :license: BSD, see LICENSE for details.
 """
 
 __import__('pkg_resources').declare_namespace(__name__)
-
--- sphinxcontrib/cldomain.py
+++ sphinxcontrib/cldomain.py
@@ -1,12 +1,25 @@
 # -*- coding: utf-8 -*-
+# cldomain is a Common Lisp domain for the Sphinx documentation tool.
+# Copyright (C) 2011-2012 Russell Sim <russell.sim@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """
     sphinx.domains.cl
     ~~~~~~~~~~~~~~~~~
 
     The Common Lisp domain
-
-    :copyright: Copyright 2011 by Russell Sim
-    :license: GPL, see LICENSE for details.
 
 
     TODO
@@ -36,6 +49,7 @@ upper_symbols = re.compile("(^|\s)([^a-z\s\"`]*[A-Z]{2,}[^a-z\s\"`:]*)($|\s)")
 
 doc_strings = {}
 args = {}
+
 
 def bool_option(arg):
     """Used to convert flag options to directives.  (Instead of
