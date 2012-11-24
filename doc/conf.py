8a91eb4940670f9236ba663b5afa2252faf61a4c
Russell Sim <russell.sim@gmail.com>
2012-11-24 17:15:51 +1100

    Ignore virtualenv files.

--- doc/conf.py
+++ doc/conf.py
@@ -80,7 +80,7 @@ release = '0.2-dev'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'bin', 'eggs', 'develop-eggs', 'parts']
+exclude_patterns = ['.venv', '_build', 'bin', 'eggs', 'develop-eggs', 'parts']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
