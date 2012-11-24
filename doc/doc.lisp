64dd3c6bb05299118a9857398f0ce4d7cf77378b
Russell Sim <russell.sim@gmail.com>
2012-11-24 16:12:03 +1100

    Added test data and some poor documentation.

---
+++ doc/doc.lisp
@@ -0,0 +1,39 @@
+;; cldomain is a Common Lisp domain for the Sphinx documentation tool.
+;; Copyright (C) 2011-2012 Russell Sim <russell.sim@gmail.com>
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(in-package :cl-user)
+
+(defpackage :sphinxcontrib.cldomain.doc
+  (:use #:common-lisp)
+  (:export #:example-function
+           #:example-function1))
+
+(in-package :sphinxcontrib.cldomain.doc)
+
+
+(defun example-function (arg1 arg2 arg3)
+  "An example function, returns a list containing ARG1 ARG2 and ARG3."
+  (list arg1 arg2 arg3))
+
+
+(defun example-function1 (arg1 arg2 &optional (arg3 #'sort))
+  "An example function similar to EXAMPLE-FUNCTION but with an
+  optional argument that contains a default with a default."
+  (list arg1 arg2 arg3))
+
+
+(defgeneric genfunc (arg1 arg2 &optional arg3)
+  (:documentation "A test generic function."))
---
+++ doc/index.rst
@@ -0,0 +1,29 @@
+cldomain
+========
+
+.. contents::
+
+About
+-----
+
+sphinxcontrib.cldomain is licensed under the GPL
+
+CLDomain is an extension for the Sphinx documentation generation tool
+that allow sphinx to generate documentation for Common Lisp libraries.
+
+Download
+--------
+
+Since there is currently no released version, the source can be
+downloaded from `github`_.
+
+.. _github: https://github.com/russell/sphinxcontrib-cldomain
+
+Repositories
+------------
+
+.. cl:package:: sphinxcontrib.cldomain.doc
+
+.. cl:function:: example-function
+
+.. cl:function:: example-function1
