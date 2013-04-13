a9bcdd5b1f0a28e2979d3026eb9c332377a685d1
Russell Sim <russell.sim@gmail.com>
2013-04-13 12:16:57 +1000

    Updated copyright.

--- doc/conf.py
+++ doc/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'cldomain'
-copyright = u'2012, Russell Sim'
+copyright = u'2011-2013, Russell Sim'
 
 googleanalytics_id = "UA-28069739-1"
 if os.environ.get("GOOGLE_ANALYTICS"):
