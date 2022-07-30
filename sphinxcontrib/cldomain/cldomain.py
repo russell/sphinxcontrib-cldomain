29143e85034f995f1df42b9dee864139ca016b1a
Russell Sim <russell.sim@gmail.com>
2022-07-30 22:31:49 +0200

    fix: dictionary changed size during iteration
    ```
    Exception occurred:
      File "/home/russell/projects/sphinxcontrib-cldomain/.direnv/python-3.10.5/lib/python3.10/site-packages/sphinxcontrib/cldomain/cldomain.py", line 924, in clear_doc
        for fullname, docs in self.data["symbols"].items():
    RuntimeError: dictionary changed size during iteration
    ```

--- sphinxcontrib/cldomain/cldomain.py
+++ sphinxcontrib/cldomain/cldomain.py
@@ -922,7 +922,7 @@ class CLDomain(Domain):
     }
 
     def clear_doc(self, docname: str) -> None:
-        for fullname, docs in self.data["symbols"].items():
+        for fullname, docs in self.data["symbols"].copy().items():
             for (fn, _ignored) in docs:
                 if fn == docname:
                     del self.data["symbols"][fullname]
